131fc3c2011d8340ee337c9d74f9e35b58516db6
Aisylu <79210517+luaisylu@users.noreply.github.com>
2023-05-21 15:50:27 +0300

    поменяла стиль кавычек

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -9,16 +9,16 @@ from download_image import download_image
 
 def fetch_spacex_last_launch(url, name_folder, args):
   params ={
-      "id": args
+      'id': args
   }
   response = requests.get(url, params=params)
   response.raise_for_status()
   images = response.json()
   image = ''.join([image for image in images])
-  if image["links"]["flickr_images"]:
-      link_the_images = image["links"]["flickr_images"]
+  if image['links']['flickr_images']:
+      link_the_images = image['links']['flickr_images']
   for images_number, link_image in enumerate(link_the_images):
-        filename = f"spacex{images_number}.jpg"
+        filename = f'spacex{images_number}.jpg'
         file_path = os.path.join(name_folder, filename)
         download_image(link_image, file_path)
 
@@ -30,9 +30,9 @@ def main():
         help='Указать свой id запуска для скачивания изображений',
         default='5eb87d42ffd86e000604b384')
     args = parser.parse_args()
-    name_folder = "media"
+    name_folder = 'media'
     Path(name_folder).mkdir(parents=True, exist_ok=True)
-    url = "https://api.spacexdata.com/v3/launches/"
+    url = 'https://api.spacexdata.com/v3/launches/'
     fetch_spacex_last_launch(url, name_folder, args)
     
     
